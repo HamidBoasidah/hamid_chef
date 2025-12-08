96f89d870b746a207053405c92d92bddd7118233
HamidBoasidah <140933334+HamidBoasidah@users.noreply.github.com>
2025-12-08 06:31:56 +0300

    Update BaseRepositoryInterface.php

--- app/Repositories/Contracts/BaseRepositoryInterface.php
+++ app/Repositories/Contracts/BaseRepositoryInterface.php
@@ -4,14 +4,23 @@ namespace App\Repositories\Contracts;
 
 interface BaseRepositoryInterface
 {
-    public function query();
-    public function all(array $with = []);
-    public function paginate(int $perPage = 10, array $with = []);
-    public function find(int|string $id, array $with = []);
-    public function findOrFail(int|string $id, array $with = []);
+    public function query(?array $with = null);
+
+    public function all(?array $with = null);
+
+    public function paginate(int $perPage = 10, ?array $with = null);
+
+    public function find(int|string $id, ?array $with = null);
+
+    public function findOrFail(int|string $id, ?array $with = null);
+
     public function create(array $attributes);
+
     public function update(int|string $id, array $attributes);
+
     public function delete(int|string $id): bool;
+
     public function activate(int|string $id);
+
     public function deactivate(int|string $id);
 }
